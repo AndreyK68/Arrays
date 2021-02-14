dc3e66fbd62cb1df63d9df9716ed8b1c14cf6dc0
Андрей Киреев <air_kireev@bk.ru>
2021-02-14 19:44:14 +0700

    Add project files.

---
+++ Arrays/Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int SIZE = 5;
+	int arr[SIZE] = { 3, 5, 8 };
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+} 
